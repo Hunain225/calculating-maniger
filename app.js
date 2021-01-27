83e13bd87e5f29fa7f9472c23b3a7936619213b7
hunain225 <hunainnaveed225@gmail.com>
2021-01-27 18:10:31 +0500

    calculater

---
+++ app.js
@@ -0,0 +1,14 @@
+function getvalue(e) {
+    var a = document.getElementById('result')
+    a.value += e
+}
+function calculate() {
+    var a = document.getElementById('result')
+    var val = a.value
+    var final = eval(val)
+    a.value = final
+}
+function clearValue() {
+    var a = document.getElementById('result')
+    a.value = ''
+}
